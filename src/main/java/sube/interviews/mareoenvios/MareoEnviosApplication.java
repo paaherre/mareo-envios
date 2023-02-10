194f05dc278da9d6cbdd41a7ae7f6b9cb1763cd4
Pablo Herrera <paaherre>
2023-02-10 12:06:15 -0300

    create project

---
+++ src/main/java/sube/interviews/mareoenvios/MareoEnviosApplication.java
@@ -0,0 +1,13 @@
+package sube.interviews.mareoenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareoEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MareoEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sube/interviews/mareoenvios/MareoEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package sube.interviews.mareoenvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MareoEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
